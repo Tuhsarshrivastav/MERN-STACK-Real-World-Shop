819069b967b24d12966b77b7f51b4adde5d77e4f
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 <ts3657@gmail.com>
2021-11-27 15:10:28 +0530

    connected with db

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const app = express();
+
+// middlewares
+app.use(express.json());
+// routes imports
+const product = require("./routes/productrRoute");
+
+app.use("api/v1/", product);
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require("./app");
 require("dotenv").config();
+const Database = require('./config/database')
+
+Database()
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
