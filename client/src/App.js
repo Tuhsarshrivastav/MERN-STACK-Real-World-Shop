0baf6dc2ad793aaec7912ee2ac3dd8ba7f80a61d
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 <ts3657@gmail.com>
2021-11-29 11:27:28 +0530

    complete footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Header from "./components/layout/Header";
+import Header from "./components/layout/header/Header";
+import Footer from "./components/layout/footer/Footer";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import WebFont from "webfontloader";
-useEffect(() => {
-  WebFont.load({
-    google: {
-      families: ["Roboto", "Droid Sans", "Chilanka"],
-    },
-  });
-}, []);
+import Home from "./components/home/Home";
 function App() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
   return (
     <BrowserRouter>
       <Header />
+      <Route path="/" exact component={Home} />
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ client/src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            hhh
+        </div>
+    )
+}
+
+export default Home
